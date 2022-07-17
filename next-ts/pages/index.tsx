3d8ee596d2d2029e7d65131eca50b6eaa4bd0dcb
Homer <hmrbcnt@gmail.com>
2022-07-17 17:45:23 +0800

    Initial commit

---
+++ next-ts/pages/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+const HomePage: FunctionComponent = () => {
+	return(
+		<div>
+			Home Page
+		</div>
+	)
+}
+
+export default HomePage;
---
+++ next-ts/pages/news.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+const NewsPage: FunctionComponent = () => {
+	return(
+		<div>
+			News Page
+		</div>
+	)
+}
+
+export default NewsPage;
