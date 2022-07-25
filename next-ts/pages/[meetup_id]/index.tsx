9827bb7c5e3d55fb130cfcbd5f84bb672a64919d
Homer <hmrbcnt@gmail.com>
2022-07-25 23:35:05 +0800

    feat: added dynamic ssg for meetupdetailspage

--- next-ts/pages/[meetup_id]/index.tsx
+++ next-ts/pages/[meetup_id]/index.tsx
@@ -2,8 +2,7 @@ import React, { FunctionComponent, useState } from 'react';
 import MeetupDetailComponent from '../../components/meetups/MeetupDetail';
 import { MeetupInterface } from '../../types/meetup';
 
-
-const MeetupDetailPage: FunctionComponent = () => {
+const MeetupDetailPage: FunctionComponent<MeetupInterface> = (props: MeetupInterface) => {
 	return (
 		<>
 			<MeetupDetailComponent 
@@ -15,6 +14,39 @@ const MeetupDetailPage: FunctionComponent = () => {
 			/>
 		</>
 	)
+}
+
+export async function getStaticPaths() {
+	// Describe all the dynamic segment values
+	// All meetup ids in this case
+	return {
+		fallback: false, // tells if your paths key contains all of your possible dynamic keys or only some of them
+		paths: [
+			{ params: {
+				// All key value pairs that might lead to your dynamic page
+				meetup_id: 'm1'
+				} 
+			} // One object per version of the dynamic key
+		]
+	}
+}
+
+export async function getStaticProps(context) {
+	// Fetch data for a single meetup
+
+	const { meetup_id } = context.params;
+
+	return {
+		props: {
+			meetupData: {
+				image: "https://www.japan-guide.com/g21/2314_02.jpg",
+				title: 'First Meetup',
+				address: 'address',
+				description: 'description',
+				id: '12345'
+			}
+		}
+	}
 }
 
 export default MeetupDetailPage;
--- next-ts/pages/index.tsx
+++ next-ts/pages/index.tsx
@@ -27,8 +27,12 @@ const DUMMY_MEETUPS = [
 	}
 ]
 
-const HomePage: FunctionComponent = () => {
-	const [loadedMeetups, setLoadedMeetups] = useState<MeetupInterface[]>([]);
+interface HomePageProps {
+	meetups: MeetupInterface[]
+}
+
+const HomePage: FunctionComponent<HomePageProps> = (props: HomePageProps) => {
+	const [loadedMeetups, setLoadedMeetups] = useState<MeetupInterface[]>(props.meetups);
 
 	useEffect(() => {
 		setLoadedMeetups(DUMMY_MEETUPS);
@@ -41,5 +45,31 @@ const HomePage: FunctionComponent = () => {
 		</>
 	)
 }
+
+
+// Executed during build process, never on client 
+// Can also access context.params, which will have the argument keys
+export async function getStaticProps() {
+	return {
+		props: {
+			meetups: DUMMY_MEETUPS
+		},
+		revalidate: 10 // # Of seconds nextjs regenerates the page after the build process
+	}
+}
+
+// // Executes on server after deployment, runs on server
+// // Runs every incoming request
+// // Only use if you have data that changes multiple times per second, or if you need to access the incoming request
+// export async function getServerSideProps(context) {
+// 	const req = context.req;
+// 	const res = context.res;
+
+// 	return {
+// 		props: {
+// 			meetups: DUMMY_MEETUPS
+// 		}
+// 	}
+// }
 
 export default HomePage;
