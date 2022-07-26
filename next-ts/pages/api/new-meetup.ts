db40d706ba4478fb7fe7466e10dc007ea9077eac
Homer <hmrbcnt@gmail.com>
2022-07-26 23:15:49 +0800

    fix: fixed a bug where fields entered for new meetup would be inserted as null

--- next-ts/pages/api/new-meetup.ts
+++ next-ts/pages/api/new-meetup.ts
@@ -9,7 +9,7 @@ import { CreateMeetupInterface } from "../../types/meetup";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	switch (req.method) {
 		case 'POST':
-			const data: CreateMeetupInterface = req.body;
+			const data: CreateMeetupInterface = JSON.parse(req.body);
 			const { image, title, address, description } = data;
 
 			const client = await MongoClient.connect('mongodb+srv://hmrbcnt:jonasbayot@fullstackopenmongodb.lqee8.mongodb.net/meetups?retryWrites=true&w=majority');
@@ -26,7 +26,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
 			client.close(); // Closing client
 
-			res.status(201).json({ message: 'Meetup created', meetup: result });
+			res.status(201).json({ message: 'Meetup created' });
 		default:
 	}
 }
