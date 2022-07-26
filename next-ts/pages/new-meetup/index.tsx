f19b45ac35d46700c93caa2fcd26161b5b3feb9b
Homer <hmrbcnt@gmail.com>
2022-07-26 22:13:12 +0800

    feat: integrated create meetup api route to nextjs page

--- next-ts/pages/new-meetup/index.tsx
+++ next-ts/pages/new-meetup/index.tsx
@@ -1,11 +1,24 @@
 import React, { FunctionComponent } from 'react';
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 import { MeetupData } from '../../types/meetup';
 
 const NewMeetup: FunctionComponent = () => {
 
-	const onAddMeetup = (meetup_data: MeetupData) => {
-		console.log(meetup_data);
+	const router = useRouter();
+
+	const onAddMeetup = async (meetup_data: MeetupData) => {
+		// Send data to api route
+		const response = await fetch('/api/new-meetup', {
+			method: 'POST',
+			body: JSON.stringify(meetup_data),
+			headers: {
+				'Contenxt-Type': 'application/json'
+			}
+		})
+
+		const data = await response.json();
+		router.push('/');
 	}
 
 	return (
